f1a71019632049b678b0d744d6b0096ac9af5ad9
leono <alumno@ulpgc.es>
2025-03-25 15:06:53 +0000

    WeatherData represents the data obtained fom the API

--- src/main/java/weather/feeder/WeatherData.java
+++ src/main/java/weather/feeder/WeatherData.java
@@ -1,4 +1,20 @@
 package weather.feeder;
 
 public class WeatherData {
+    private final String city;
+    private final double temperature;
+    private final double humidity;
+    private final String description;
+
+    public WeatherData(String city, double temperature, double humidity, String description) {
+        this.city = city;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.description = description;
+    }
+    public String getCity() { return city; }
+    public double getTemperature() { return temperature; }
+    public double getHumidity() { return humidity; }
+    public String getDescription() { return description; }
+
 }
